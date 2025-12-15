d755ad2253baff3c2d3f84d4263bed8a0d7d84f0
Hamada <k024g0103@g.neec.ac.jp>
2025-12-15 12:38:36 +0900

    templateを使っていなかったのを訂正

--- 02-01/main.cpp
+++ 02-01/main.cpp
@@ -1,15 +1,8 @@
-#include "Windows.h"
+#include <Windows.h>
 #include <iostream>
 
-int Min(int n1,int n2) {
-	return (n1 < n2) ? n1 : n2;
-}
-
-float Min(float n1, float n2) {
-	return (n1 < n2) ? n1 : n2;
-}
-
-double Min(double n1, double n2) {
+template<typename T>
+T Min(T n1, T n2) {
 	return (n1 < n2) ? n1 : n2;
 }
 
