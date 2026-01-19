8c11092ce6f7f6a61a0e3a44780366ed260fab0d
Hamada <k024g0103@g.neec.ac.jp>
2026-01-19 10:38:11 +0900

    02-01訂正

--- 02-01/main.cpp
+++ 02-01/main.cpp
@@ -7,9 +7,17 @@ T Min(T n1, T n2) {
 }
 
 int main() {
-	printf_s("Min(10, 20) = %d\n", Min(10, 20));
-	printf_s("Min(10.5f, 20.5f) = %.2f\n", Min(10.5f, 20.5f));
-	printf_s("Min(10.5, 20.5) = %.2f\n", Min(10.5, 20.5));
+    // int型のペア
+    int i1 = 10, i2 = 20;
+    printf_s("Min(%d, %d) = %d\n", i1, i2, Min(i1, i2));
+
+    // float型のペア
+    float f1 = 10.5f, f2 = 20.5f;
+    printf_s("Min(%.1ff, %.1ff) = %.2ff\n", f1, f2, Min(f1, f2));
+
+    // double型のペア
+    double d1 = 10.5, d2 = 20.5;
+    printf_s("Min(%.1f, %.1f) = %.2f\n", d1, d2, Min(d1, d2));
 
 	return 0;
 }
